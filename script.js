66c88e57ba9b41cb6b57fa870480763bc78b754e
Collappo <123423293+Collappo@users.noreply.github.com>
2025-10-01 23:14:38 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+const color = document.getElementById("color")
+const rangeInput = document.querySelectorAll(".input-range")
+const valueInput = document.querySelectorAll(".input-value")
+
+function RGBToHex(red, green, blue) {
+    red = red.toString(16)
+    if (red.length === 1) { red = "0" + red }
+    green = green.toString(16)
+    if (green.length === 1) { green = "0" + green }
+    blue = blue.toString(16)
+    if (blue.length === 1) { blue = "0" + blue }
+    let hex = `#${red}${green}${blue}`
+    return hex
+}
+
+rangeInput.forEach((e, i) => {
+    e.value = "255"
+    valueInput[i].value = "255"
+    e.addEventListener("input", () => {
+        valueInput[i].value = e.value
+        color.style.background = RGBToHex(parseInt(rangeInput[0].value), parseInt(rangeInput[1].value), parseInt(rangeInput[2].value))
+    })
+
+    valueInput[i].addEventListener("input", () => {
+        e.value = valueInput[i].value
+        color.style.background = RGBToHex(parseInt(valueInput[0].value), parseInt(valueInput[1].value), parseInt(valueInput[2].value))
+    })
+})
+
