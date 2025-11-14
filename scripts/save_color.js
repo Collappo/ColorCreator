d00c0859946cbb9f7376692fbc1cb52cde7272e8
Collappo <123423293+Collappo@users.noreply.github.com>
2025-11-14 10:40:13 +0100

    Poprawa save_color.js

--- scripts/save_color.js
+++ scripts/save_color.js
@@ -1,7 +1,8 @@
 const saveButton = document.getElementById("save-color-value")
 
+let amountOfColors = parseInt(localStorage.getItem("amount-of-colors"))
+
 saveButton.addEventListener("click", () => {
-    let amountOfColors = parseInt(localStorage.getItem("amount-of-colors"))
     let colorToSave = colorValue.value
     let flag = false
     for (let i = 0; i <= amountOfColors; i++) {
@@ -13,11 +14,12 @@ saveButton.addEventListener("click", () => {
     if (!flag) {
         localStorage.setItem(`color-${amountOfColors}`, colorToSave)
         localStorage.setItem("amount-of-colors", (amountOfColors + 1).toString())
-        refreshColorsList()
+        listColors()
         showNotification("SAVED!", true, saveButton)
     }
 
     else {
         showNotification("Color can't be saved!", false, saveButton)
     }
+
 })
